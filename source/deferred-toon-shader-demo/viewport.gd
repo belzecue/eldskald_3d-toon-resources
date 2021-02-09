8ebf2dd92f32752d5a44617e562f9e6d5cbc0058
eldskald <rafaelbordoni00@gmail.com>
2021-02-09 20:40:20 -0300

    Delete unused files

--- source/deferred-toon-shader-demo/viewport.gd
+++
@@ -1,7 +0,0 @@
-extends Viewport
-
-export (Array, NodePath) var viewports
-
-func _on_Viewport_size_changed():
-	for path in viewports:
-		get_node(path).size = self.size
--- source/rotate.gd
+++
@@ -1,4 +0,0 @@
-extends MeshInstance
-
-func _process(delta):
-	self.rotation_degrees.y += delta*20
